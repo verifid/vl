a241f308c2712c3e74aaaa6406eda5186c3ef9db
Abdullah Selek <abdullahselek@gmail.com>
2018-12-11 18:11:08 +0000

    Start implementing api.

---
+++ vl/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Self deployed identity verification layer."""
+
+from __future__ import absolute_import
+
+__author__       = 'Abdullah Selek'
+__email__        = 'abdullahselek@gmail.com'
+__copyright__    = 'Copyright (c) 2018 Abdullah Selek'
+__license__      = 'MIT License'
+__version__      = '0.1.1'
+__url__          = 'https://github.com/verifid/vl'
+__download_url__ = 'https://github.com/verifid/vl'
+__description__  = 'Self deployed identity verification layer.'
