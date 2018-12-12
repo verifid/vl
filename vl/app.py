c81bffa1d7b942db02d3ad0ed7c7b1dac3705b71
Abdullah Selek <abdullahselek@gmail.com>
2018-12-12 21:43:57 +0000

    Get & set configuration properties.

--- vl/app.py
+++ vl/app.py
@@ -5,3 +5,12 @@ import os
 import tornado.ioloop
 import tornado.web
 import json
+
+try:
+    scheme = os.environ['SCHEME']
+    ip_address = os.environ['IP_ADDRESS']
+    port = os.environ['PORT']
+except KeyError:
+    scheme = 'http'
+    ip_address = 'localhost'
+    port = 80
