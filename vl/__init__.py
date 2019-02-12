78c4cfd205c6fc4d9e5973c38feb322ba516994f
Abdullah Selek <abdullahselek@gmail.com>
2019-02-12 21:46:03 +0000

    Update copyright.

--- vl/__init__.py
+++ vl/__init__.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 
 __author__       = 'Abdullah Selek'
 __email__        = 'abdullahselek@gmail.com'
-__copyright__    = 'Copyright (c) 2018 Abdullah Selek'
+__copyright__    = 'Copyright (c) 2019 Abdullah Selek'
 __license__      = 'MIT License'
 __version__      = '0.1'
 __url__          = 'https://github.com/verifid/vl'
