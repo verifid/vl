09f69791587fed0749af6012cafa5e3425a385b5
Abdullah Selek <abdullahselek@gmail.com>
2019-03-01 14:42:15 +0000

    Reimplement user controller.

--- vl-flask/swagger_server/controllers/user_controller.py
+++ vl-flask/swagger_server/controllers/user_controller.py
@@ -1,20 +1,62 @@
+import os
 import connexion
 import six
+import uuid
+import asyncio
 
-from swagger_server.models.user import User  # noqa: E501
+from swagger_server.store.redis_store import RedisStore
+from swagger_server.models.user import User
 from swagger_server import util
+from swagger_server.models.api_response import ApiResponse
+from redis import Redis
+from facereg import google_images
 
+redis = Redis(host='localhost', port=6379)
+store = RedisStore(redis)
 
-def user_data(body):  # noqa: E501
+json_model = ['name', 'surname', 'gender',
+            'date_of_birth', 'place_of_birth', 'country']
+
+def validate_json(body):
+    if set(body) != set(json_model):
+        return False
+    for key in json_model:
+        if body[key] == None:
+            return False
+    return True
+
+def user_id():
+    return str(uuid.uuid4())
+
+@asyncio.coroutine
+def download_images(name, surname, user_id):
+    output_directory = os.getcwd() + '/datasets/' + user_id
+    _, _ = google_images.download(str.format('{0} {1}', name, surname),
+                            limit=3, output_directory=output_directory)
+
+def user_data(body):
     """Creates a user for verification.
-
-     # noqa: E501
 
     :param body: User object that needs to be added temporarly
     :type body: dict | bytes
 
     :rtype: None
     """
+
+    print('REQUEST : ', connexion.request)
     if connexion.request.is_json:
-        body = User.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        if validate_json(body) == False:
+            api_response = ApiResponse(code=400, type='error', message='Request has missing values.')
+            return api_response.to_str()
+        else:
+            body = User.from_dict(connexion.request.get_json())
+            user = user_id()
+            yield download_images(body.name, body.surname, uuid)
+            store.keep(uuid, body)
+            api_response = ApiResponse.from_dict({'code': 200, 'type': 'sucess', 
+                                        'message': 'User created with received values.',
+                                        'userId': user})
+            return api_response.to_str()
+    else:
+        api_response = ApiResponse(code=400, type='error', message='Request needs a user json object.')
+        return api_response.to_str()
