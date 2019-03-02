703a5adfed530905ab3dec3427299647eb0ca70e
Abdullah Selek <abdullahselek@gmail.com>
2019-03-02 23:39:21 +0000

    Changes for image controller.

--- vl-flask/swagger_server/controllers/image_controller.py
+++ vl-flask/swagger_server/controllers/image_controller.py
@@ -1,20 +1,43 @@
+import os
 import connexion
 import six
 
-from swagger_server.models.api_response import ApiResponse  # noqa: E501
+from swagger_server.models.api_response import ApiResponse
 from swagger_server import util
+from swagger_server.store.redis_store import RedisStore
+from redis import Redis
+from enum import Enum
 
+redis = Redis(host='localhost', port=6379)
+store = RedisStore(redis)
 
-def upload_image(userId, file):  # noqa: E501
-    """Uploads an image
+def save_image(user_id, file):
+     dir = os.getcwd() + '/testsets/' + user_id + '/'
+     os.mkdir(dir)
+     file_path = dir + file.name
+     file.save(file_path, buffer_size=16384)
+     file.close()
 
-     # noqa: E501
+def upload_image(userId, file):
+    """Uploads an image.
 
-    :param userId: ID of user to update
+    :param userId: ID of user to update.
     :type userId: str
     :param file: Image file to upload.
     :type file: werkzeug.datastructures.FileStorage
 
     :rtype: ApiResponse
     """
-    return 'do some magic!'
+
+    if not userId:
+        api_response = ApiResponse.from_dict({'code': 400, 'type': 'error',
+                                        'message': 'No user id found on request.'})
+        return api_response.to_str()
+    if not file:
+        api_response = ApiResponse.from_dict({'code': 412, 'type': 'error',
+                                        'message': 'No file found on request.'})
+        return api_response.to_str()
+    save_image(userId, file)
+    api_response = ApiResponse.from_dict({'code': 200, 'type': 'success',
+                                        'message': 'Image file received.'})
+    return api_response.to_str()
