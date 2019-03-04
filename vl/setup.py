02b11cead16160793e558888979bcbe3d4f0d63a
Abdullah Selek <abdullahselek@gmail.com>
2019-03-04 18:38:17 +0000

    Fix new module issues.

--- vl/setup.py
+++ vl/setup.py
@@ -6,13 +6,6 @@ from setuptools import setup, find_packages
 NAME = "vl"
 VERSION = "1.0.0"
 
-# To install the library, run the following
-#
-# python setup.py install
-#
-# prerequisite: setuptools
-# http://pypi.python.org/pypi/setuptools
-
 REQUIRES = ["connexion"]
 
 setup(
