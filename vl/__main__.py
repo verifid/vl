f103ad89c5c9e1067512b829c896be946affaa9d
Abdullah Selek <abdullahselek@gmail.com>
2019-03-04 18:05:10 +0000

    Use default port.

--- vl/__main__.py
+++ vl/__main__.py
@@ -9,7 +9,7 @@ app = connexion.App(__name__, specification_dir='./swagger/')
 def main():
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Identity Verification Layer'})
-    app.run(port=80)
+    app.run(port=5000)
 
 if __name__ == '__main__':
     main()
