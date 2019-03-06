9586f4423ff17af018b2b2fc76624f337fa9a7bc
Abdullah Selek <abdullahselek@gmail.com>
2019-03-06 22:05:47 +0000

    Introduce upload identity image endpoint.

--- test/test_image_controller.py
+++ test/test_image_controller.py
@@ -11,32 +11,64 @@ from . import BaseTestCase
 class TestImageController(BaseTestCase):
     """ImageController integration test stubs"""
 
-    def test_upload_image_success(self):
-        """Test success case for upload_file.
+    def test_upload_identity_success(self):
+        """Test success case for upload identity.
 
-        Uploads an image
+        Uploads an identity image.
         """
 
         data = dict(userId='userId_example',
                     file=(BytesIO(b'some file data'), 'test.png'))
         response = self.client.open(
-            '/v1/image/upload',
+            '/v1/image/uploadIdentity',
             method='POST',
             data=data,
             content_type='multipart/form-data')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_upload_image_fail(self):
-        """Test fail case for upload_file.
+    def test_upload_identity_fail(self):
+        """Test fail case for upload identity.
 
-        Uploads an image
+        Uploads an identity image and fails.
         """
 
         data = dict(userId='userId_example',
                     file=None)
         response = self.client.open(
-            '/v1/image/upload',
+            '/v1/image/uploadIdentity',
+            method='POST',
+            data=data,
+            content_type='multipart/form-data')
+        self.assert400(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_upload_profile_success(self):
+        """Test success case for upload profile.
+
+        Uploads a profile image.
+        """
+
+        data = dict(userId='userId_example',
+                    file=(BytesIO(b'some file data'), 'test.png'))
+        response = self.client.open(
+            '/v1/image/uploadProfile',
+            method='POST',
+            data=data,
+            content_type='multipart/form-data')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_upload_profile_fail(self):
+        """Test fail case for upload identity.
+
+        Uploads an profile image and fails.
+        """
+
+        data = dict(userId='userId_example',
+                    file=None)
+        response = self.client.open(
+            '/v1/image/uploadProfile',
             method='POST',
             data=data,
             content_type='multipart/form-data')
--- vl/controllers/image_controller.py
+++ vl/controllers/image_controller.py
@@ -11,20 +11,24 @@ from enum import Enum
 redis = Redis(host='localhost', port=6379)
 store = RedisStore(redis)
 
-def save_image(user_id, file):
-     directory = os.getcwd() + '/testsets/' + user_id + '/'
-     if not os.path.exists(directory):
+def save_image(user_id, file, identity):
+    if identity:
+        path = 'identity/'
+    else:
+        path = 'profile/'
+    directory = os.getcwd() + '/testsets/' + path +  user_id + '/'
+    if not os.path.exists(directory):
         os.makedirs(directory)
-     file_path = directory + file.filename
-     file.save(file_path, buffer_size=16384)
-     file.close()
+    file_path = directory + file.filename
+    file.save(file_path, buffer_size=16384)
+    file.close()
 
-def upload_image(userId, file):
-    """Uploads an image.
+def upload_identity(userId, file):
+    """Uploads an identity card image.
 
     :param userId: ID of user to update.
     :type userId: str
-    :param file: Image file to upload.
+    :param file: Identity picture to upload.
     :type file: werkzeug.datastructures.FileStorage
 
     :rtype: ApiResponse
@@ -38,7 +42,31 @@ def upload_image(userId, file):
         api_response = ApiResponse.from_dict({'code': 412, 'type': 'error',
                                         'message': 'No file found on request.'})
         return api_response.to_str()
-    save_image(userId, file)
+    save_image(userId, file, identity=True)
+    api_response = ApiResponse.from_dict({'code': 200, 'type': 'success',
+                                        'message': 'Image file received.'})
+    return api_response.to_str()
+
+def upload_profile(userId, file):
+    """Uploads a profile image.
+
+    :param userId: ID of user to update.
+    :type userId: str
+    :param file: Profile picture to upload.
+    :type file: werkzeug.datastructures.FileStorage
+
+    :rtype: ApiResponse
+    """
+
+    if not userId:
+        api_response = ApiResponse.from_dict({'code': 400, 'type': 'error',
+                                        'message': 'No user id found on request.'})
+        return api_response.to_str()
+    if not file:
+        api_response = ApiResponse.from_dict({'code': 412, 'type': 'error',
+                                        'message': 'No file found on request.'})
+        return api_response.to_str()
+    save_image(userId, file, identity=False)
     api_response = ApiResponse.from_dict({'code': 200, 'type': 'success',
                                         'message': 'Image file received.'})
     return api_response.to_str()
