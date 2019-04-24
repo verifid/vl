0e3b50f12099d7b5283f17ccbbb62c47cedc7996
Abdullah Selek <abdullahselek@gmail.com>
2019-04-24 12:25:05 +0100

    Get optical characters from identity image.

--- vl/controllers/user_controller.py
+++ vl/controllers/user_controller.py
@@ -89,6 +89,12 @@ def verify(body):
         image_path = os.getcwd() + '/testsets/' + 'identity' + '/' + user_id + '/' + 'image.png'
         east_path = os.getcwd() + '/vl' + '/' + 'model/frozen_east_text_detection.pb'
         text_recognizer = TextRecognizer(image_path, east_path)
+        (image, _, _) = text_recognizer.load_image()
+        (resized_image, ratio_height, ratio_width, _, _) = text_recognizer.resize_image(image, 320, 320)
+        (scores, geometry) = text_recognizer.geometry_score(east_path, resized_image)
+        boxes = text_recognizer.boxes(scores, geometry)
+        results = text_recognizer.get_results(boxes, image, ratio_height, ratio_width)
+        print(results)
         response = jsonify({'code': 200, 'type': 'success',
                                 'message': 'Given user has verified!'})
         response.status_code = 200
