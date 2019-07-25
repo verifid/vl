930f401c4e4b580acada31f67ce016a354bc794c
Abdullah Selek <abdullahselek@gmail.com>
2019-07-25 09:36:14 +0100

    Combine controllers.

--- tests/test_default_controller.py
+++ tests/test_default_controller.py
@@ -2,9 +2,14 @@
 
 from __future__ import absolute_import
 
+import os
+import fakeredis
+
 from flask import json
 from six import BytesIO
+from shutil import copy2
 
+from vl.store.redis_store import RedisStore
 from vl.models.api_response import ApiResponse  # noqa: E501
 from vl.models.error import Error  # noqa: E501
 from vl.models.user import User  # noqa: E501
@@ -17,12 +22,14 @@ from . import BaseTestCase
 class TestDefaultController(BaseTestCase):
     """DefaultController integration test stubs"""
 
-    def test_send_user_data(self):
+
+    def test_send_user_data_success(self):
         """Test case for send_user_data
-
-
         """
-        body = User()
+        body = User(name='Tony',
+                    surname='Stark',
+                    date_of_birth='1980-10-01T00:00:00Z',
+                    country='USA')
         response = self.client.open(
             '/v1/user/sendUserData',
             method='POST',
@@ -31,13 +38,103 @@ class TestDefaultController(BaseTestCase):
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_upload_identity(self):
-        """Test case for upload_identity
 
-
+    def test_send_user_data_fail(self):
+        """Test case for send_user_data
         """
-        data = dict(user_id='user_id_example',
-                    identity_image='identity_image_example')
+        response = self.client.open(
+            '/v1/user/sendUserData',
+            method='POST',
+            data=None,
+            content_type='application/json')
+        self.assert400(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+    def test_send_user_data_fail_with_missing_value(self):
+        """Test case for send_user_data
+        """
+        body = User(name='Tony',
+                    surname=None,
+                    date_of_birth='1980-10-01T00:00:00Z',
+                    country='USA')
+        response = self.client.open(
+            '/v1/user/sendUserData',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert400(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+    def test_verify_fail_with_invalid_user(self):
+        """Test case for verify
+
+        Verifies user.
+        """
+        body = UserId()
+        body.user_id = 'userId'
+        response = self.client.open(
+            '/v1/user/verify',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert400(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+    def test_verify_success_with_valid_user(self):
+        """Test case for verify
+
+        Verifies user.
+        """
+        user_id = 'userId'
+        directory = os.getcwd() + '/testsets/' + 'identity' + '/' + user_id + '/'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        src_image_path = os.path.dirname(os.path.realpath(__file__)) + '/resources/sample_uk_identity_card.png'
+        copy2(src_image_path, directory)
+        os.rename(directory + '/sample_uk_identity_card.png', directory + 'image.png')
+
+        body = UserId()
+        body.user_id = user_id
+        body.language = 'en_core_web_sm'
+        redis = fakeredis.FakeStrictRedis()
+        store = RedisStore(redis)
+        user = self.create_user()
+        store.keep(user_id, json.dumps(user))
+        response = self.client.open(
+            '/v1/user/verify',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+    def create_user(self):
+        user = User()
+        user.name = 'Elizabeth'
+        user.surname = 'Green'
+        user.country = 'United Kingdom'
+        user.date_of_birth = '14.04.1977'
+        return user
+
+
+    def test_upload_identity_success(self):
+        """Test success case for upload identity.
+
+        Uploads an identity image.
+        """
+        redis = fakeredis.FakeStrictRedis()
+        store = RedisStore(redis)
+        store.keep('userId', 'user')
+        image_path = os.path.dirname(os.path.realpath(__file__)) + '/resources/sample_uk_identity_card.png'
+        with open(image_path, 'rb') as f:
+            image_data = BytesIO(f.read())
+        data = dict(userId='userId',
+                    file=(image_data, 'image.png'))
         response = self.client.open(
             '/v1/image/uploadIdentity',
             method='POST',
@@ -46,13 +143,33 @@ class TestDefaultController(BaseTestCase):
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_upload_profile(self):
-        """Test case for upload_profile
 
+    def test_upload_identity_fail(self):
+        """Test fail case for upload identity.
 
+        Uploads an identity image and fails.
         """
-        data = dict(user_id='user_id_example',
-                    profile_image='profile_image_example')
+        data = dict(userId='userId_example',
+                    file=None)
+        response = self.client.open(
+            '/v1/image/uploadIdentity',
+            method='POST',
+            data=data,
+            content_type='multipart/form-data')
+        self.assert400(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+    def test_upload_profile_success(self):
+        """Test success case for upload profile.
+
+        Uploads a profile image.
+        """
+        redis = fakeredis.FakeStrictRedis()
+        store = RedisStore(redis)
+        store.keep('userId', 'user')
+        data = dict(userId='userId',
+                    file=(BytesIO(b'some file data'), 'test.png'))
         response = self.client.open(
             '/v1/image/uploadProfile',
             method='POST',
@@ -61,18 +178,20 @@ class TestDefaultController(BaseTestCase):
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_verify(self):
-        """Test case for verify
 
+    def test_upload_profile_fail(self):
+        """Test fail case for upload identity.
 
+        Uploads an profile image and fails.
         """
-        body = UserId()
+        data = dict(userId='userId_example',
+                    file=None)
         response = self.client.open(
-            '/v1/user/verify',
+            '/v1/image/uploadProfile',
             method='POST',
-            data=json.dumps(body),
-            content_type='application/json')
-        self.assert200(response,
+            data=data,
+            content_type='multipart/form-data')
+        self.assert400(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
 
