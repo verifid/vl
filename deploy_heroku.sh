f8e720809a6ef5297de44b2c69cc231143d7b4aa
Abdullah Selek <abdullahselek@gmail.com>
2019-08-20 08:57:51 +0100

    Enable continuous deployment.

---
+++ deploy_heroku.sh
@@ -0,0 +1,5 @@
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install @heroku-cli/plugin-container-registry
+docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APP_NAME
+heroku container:release web --app $HEROKU_APP_NAME
